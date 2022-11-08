5b6cb02ea6a4a62f3b392a4c8c4bff7f1a7b27b1
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2022-11-08 14:48:13 +0500

    initial.commit

---
+++ jss/customer.js
@@ -0,0 +1,6 @@
+var elBody = document.querySelector("body");
+var elModeBtn = document.querySelector(".mode");
+
+elModeBtn.addEventListener("click", function () {
+    elBody.classList.toggle("dark");
+});
